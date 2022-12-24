d57288826b7fb7eda91905299719ea9a5d70e465
elioflo <eliofloreschoque2357@gmail.com>
2022-12-23 23:35:56 -0300

    Add tablet version

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,24 @@
 function validateForm(e){
     e.preventDefault();
-    const form = e.target
-    const input = form.getElementsByTagName('input')[0]
-    let message = "Unknown error ocurred!"
+    const input = document.getElementById('email')
+    const message = document.getElementById('email-validation')
+    let errorMessage = "Unknown error ocurred!"
     if(input.validity.typeMismatch){
-        message = "Please provide a valid email address"
+        errorMessage = "Please provide a valid email address"
     }
 
     if(input.validity.valueMissing){
-        message = "Whoops! It looks like you forgot to add your email"
+        errorMessage = "Whoops! It looks like you forgot to add your email"
     }
-
-    if(!input.checkValidity()){
-        document.getElementById('email-validation').textContent = message
+    
+    if(input.checkValidity()){
+        message.classList.remove('container__message--invalid')
+        message.classList.add('container__message--hidden')
+        input.classList.remove('container__input--invalid')
+    }else{
+        message.textContent = errorMessage
+        message.classList.remove('container__message--hidden')
+        message.classList.add('container__message--invalid')
         input.classList.add('container__input--invalid')
     }
 }
